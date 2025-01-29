6ae171e7e5ad99e4d2b35c2806c09a941b9da9a3
joncrall <erotemic@gmail.com>
2025-01-29 16:57:10 -0500

    Add docstr

--- yolo/utils/kwcoco_utils.py
+++ yolo/utils/kwcoco_utils.py
@@ -6,6 +6,17 @@ Helpers for COCO / KWCoco integration
 def tensor_to_kwimage(yolo_annot_tensor):
     """
     Convert a raw output tensor to a kwimage Detections object
+
+    Args:
+        yolo_annot_tensor (Tensor):
+            Each row corresponds to an annotation.
+            yolo_annot_tensor[:, 0] is the class index
+            yolo_annot_tensor[:, 1:5] is the ltrb bounding box
+            yolo_annot_tensor[:, 5] is the objectness confidence
+            Other columns are the per-class confidence
+
+    Example:
+        yolo_annot_tensor = torch.rand(1, 6)
     """
     import kwimage
     class_idxs = yolo_annot_tensor[:, 0].int()
