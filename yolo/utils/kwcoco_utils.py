3836c5dfe7d8b34684bbe78917b01cecd177ea84
joncrall <erotemic@gmail.com>
2025-02-02 21:31:14 -0500

    fix: kwcoco file now properly writes at the end of inference.

--- yolo/utils/kwcoco_utils.py
+++ yolo/utils/kwcoco_utils.py
@@ -3,7 +3,7 @@ Helpers for COCO / KWCoco integration
 """
 
 
-def tensor_to_kwimage(yolo_annot_tensor):
+def tensor_to_kwimage(yolo_annot_tensor, classes=None):
     """
     Convert a raw output tensor to a kwimage Detections object
 
@@ -15,6 +15,9 @@ def tensor_to_kwimage(yolo_annot_tensor):
             yolo_annot_tensor[:, 5] is the objectness confidence
             Other columns are the per-class confidence
 
+        classes (kwcoco.CategoryTree):
+            ...
+
     Example:
         yolo_annot_tensor = torch.rand(1, 6)
     """
@@ -23,10 +26,17 @@ def tensor_to_kwimage(yolo_annot_tensor):
     boxes = kwimage.Boxes(yolo_annot_tensor[:, 1:5], format='xyxy')
     dets = kwimage.Detections(
         boxes=boxes,
-        class_idxs=class_idxs
+        class_idxs=class_idxs,
+        classes=classes,
     )
 
     if yolo_annot_tensor.shape[1] > 5:
         scores = yolo_annot_tensor[:, 5]
         dets.data['scores'] = scores
+
+    if classes is not None:
+        import torch
+        idx_to_id = torch.Tensor(classes.idx_to_id).int().to(class_idxs.device)
+        class_ids = idx_to_id[class_idxs]
+        dets.data['class_ids'] = class_ids
     return dets
